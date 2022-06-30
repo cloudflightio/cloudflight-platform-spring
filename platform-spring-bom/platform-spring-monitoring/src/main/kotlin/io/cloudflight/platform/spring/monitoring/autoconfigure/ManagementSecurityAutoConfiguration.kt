b21b70ad9c56f39d27f2e0703bf8932e89f8d0a9
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-06-30 09:58:57 +0200

    remove deprecated code around spring security (#13)

--- platform-spring-bom/platform-spring-monitoring/src/main/kotlin/io/cloudflight/platform/spring/monitoring/autoconfigure/ManagementSecurityAutoConfiguration.kt
+++ platform-spring-bom/platform-spring-monitoring/src/main/kotlin/io/cloudflight/platform/spring/monitoring/autoconfigure/ManagementSecurityAutoConfiguration.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.env.EnvironmentPostProcessor
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
@@ -12,22 +13,18 @@ import org.springframework.core.env.ConfigurableEnvironment
 import org.springframework.core.env.MapPropertySource
 import org.springframework.core.env.Profiles
 import org.springframework.security.config.annotation.web.builders.WebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 
 @Configuration
-@ConditionalOnClass(WebSecurityConfigurerAdapter::class)
+@ConditionalOnClass(WebSecurity::class)
 class ManagementSecurityAutoConfiguration {
 
     /**
      * Deactivates Spring Security for all management endpoints
      */
-    @Order(Ordered.LOWEST_PRECEDENCE)
-    @Configuration(proxyBeanMethods = false)
-    class ActuatorSecurity : WebSecurityConfigurerAdapter() {
-
-        override fun configure(web: WebSecurity) {
-            web.ignoring().requestMatchers(EndpointRequest.toAnyEndpoint())
-        }
+    @Bean
+    fun managementEndpointCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web -> web.ignoring().requestMatchers(EndpointRequest.toAnyEndpoint()) }
     }
 }
 
