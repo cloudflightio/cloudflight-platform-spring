b21b9861479a20413c4bd2fb97880f4eedf3507b
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-08-02 07:03:18 +0200

    Kl/update spring (#16)
    * add javadoc
    
    * update gradle
    
    * update autoconfigure-gradle
    
    * update spring boot and archunit-ccv

--- platform-spring-test-bom/platform-spring-test-archunit/src/main/kotlin/io/cloudflight/platform/spring/test/archunit/AbstractCleanCodeTest.kt
+++ platform-spring-test-bom/platform-spring-test-archunit/src/main/kotlin/io/cloudflight/platform/spring/test/archunit/AbstractCleanCodeTest.kt
@@ -4,6 +4,10 @@ import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.junit.ArchTests
 import io.cloudflight.cleancode.archunit.CleanCodeRuleSets
 
+/**
+ * Extend from this test case and create your own ArchitectureTest
+ * in order to automatically get all rules from https://github.com/cloudflightio/archunit-cleancode-verifier
+ */
 abstract class AbstractCleanCodeTest {
     @ArchTest
     val cleancode = ArchTests.`in`(CleanCodeRuleSets::class.java)
