574c7e7c58925feb89b0c7f71e641629f99440c0
Harald Radi <harald.radi@catalysts.cc>
2022-09-26 22:06:58 +0200

    Kotlin data classes are final, nevertheless they can be deserialized … (#32)
    * Kotlin data classes are final, nevertheless they can be deserialized with the help of jackson-module-kotlin. In order to do so type information always has to be serialized to the redis cache.
    
    Co-authored-by: Harald Radi <harald.radi@cloudflight.io>

--- platform-spring-bom/platform-spring-caching/src/main/kotlin/io/cloudflight/platform/spring/caching/autoconfigure/CachingAutoConfiguration.kt
+++ platform-spring-bom/platform-spring-caching/src/main/kotlin/io/cloudflight/platform/spring/caching/autoconfigure/CachingAutoConfiguration.kt
@@ -111,14 +111,14 @@ class CachingAutoConfiguration {
         private val JSON = createJsonSerializer()
         private val LOG = KotlinLogging.logger { }
 
-        private fun createJsonSerializer(): GenericJackson2JsonRedisSerializer {
+        internal fun createJsonSerializer(): GenericJackson2JsonRedisSerializer {
             val objectMapper = ObjectMapperFactory
                 .createObjectMapper()
                 .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
 
             objectMapper.activateDefaultTyping(
                 objectMapper.polymorphicTypeValidator,
-                ObjectMapper.DefaultTyping.NON_FINAL,
+                ObjectMapper.DefaultTyping.EVERYTHING,
                 JsonTypeInfo.As.PROPERTY
             )
 
