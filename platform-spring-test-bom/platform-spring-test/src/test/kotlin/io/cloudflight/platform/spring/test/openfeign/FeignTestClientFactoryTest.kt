d3494901850d1cb5e66bcb9a461754da2aa92af1
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-11-01 15:02:03 +0100

    fix deprecated import

--- platform-spring-test-bom/platform-spring-test/src/test/kotlin/io/cloudflight/platform/spring/test/openfeign/FeignTestClientFactoryTest.kt
+++ platform-spring-test-bom/platform-spring-test/src/test/kotlin/io/cloudflight/platform/spring/test/openfeign/FeignTestClientFactoryTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
