76ea75adccc76339c015590a7ee1ac005e96ee2b
Yannick Strehlow <ystrehlow@proton.me>
2023-07-20 18:29:07 +0200

    Fix nullability discrepancy between interface and implementation

--- platform-spring-bom/platform-spring-scheduling/src/main/kotlin/io/cloudflight/platform/spring/scheduling/lock/NoopLock.kt
+++ platform-spring-bom/platform-spring-scheduling/src/main/kotlin/io/cloudflight/platform/spring/scheduling/lock/NoopLock.kt
@@ -10,12 +10,12 @@ import java.util.*
  * @author Harald Radi (harald.radi@catalysts.cc)
  * @version 1.0
  */
-class NoopLock(lockConfiguration: LockConfiguration?) : AbstractSimpleLock(lockConfiguration) {
+class NoopLock(lockConfiguration: LockConfiguration) : AbstractSimpleLock(lockConfiguration) {
     @Suppress("EmptyFunctionBlock")
     override fun doUnlock() {
     }
 
-    override fun doExtend(newConfig: LockConfiguration?): Optional<SimpleLock> {
+    override fun doExtend(newConfig: LockConfiguration): Optional<SimpleLock> {
         return Optional.of(this)
     }
 }
